1221aa9df8a760dfe242c4918ae8c273711e0e76
omarsalim <83906211+Everezze@users.noreply.github.com>
2024-02-14 18:11:51 +0100

    project done, readme left

---
+++ script.js
@@ -0,0 +1,15 @@
+const button =  document.querySelector("button");
+const title = document.querySelector("h1");
+const paragraph = document.querySelector("p");
+
+button.addEventListener("click",function(){
+    const advice = fetch("https://api.adviceslip.com/advice", {cache: "no-store"});
+    const response = advice.then(value => value.json());
+    const data = response.then(value => value["slip"]);
+    console.log(data);
+    data.then(value => {
+        title.textContent =  `advice #${value["id"]}`;
+        paragraph.textContent =  `${value["advice"]}`;
+        return value;
+    });
+});
